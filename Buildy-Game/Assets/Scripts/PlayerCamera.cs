35ea8dd10d3d31ba1f97c2999bc5927e86aee890
Shafournee <31718336+Shafournee@users.noreply.github.com>
2018-06-04 22:42:56 -0400

    Added a player and bad movement

---
+++ Buildy-Game/Assets/Scripts/PlayerCamera.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerCamera : MonoBehaviour {
+
+    [SerializeField] GameObject player;
+
+    [SerializeField] float horizontalSpeed = 1f;
+    [SerializeField] float verticalSpeed = 1f;
+
+    public float yaw = 0.0f;
+    private float pitch = 0.0f;
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (player.GetComponent<Player>().playerCanMove)
+        {
+            //TODO move the camera position up
+            transform.position = player.GetComponent<Transform>().position;
+            CameraRotation();
+        }
+
+    }
+
+    void CameraRotation()
+    {
+        if (pitch >= 90)
+        {
+            pitch = 90;
+        }
+        else if (pitch <= -90)
+        {
+            pitch = -90;
+        }
+        yaw += horizontalSpeed * Input.GetAxis("Mouse X");
+        pitch -= verticalSpeed * Input.GetAxis("Mouse Y");
+        transform.eulerAngles = new Vector3(pitch, yaw, 0);
+    }
+}
